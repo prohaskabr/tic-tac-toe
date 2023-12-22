7d6fad42d25a3fb757be2092fd1388bf9b1c5640
Thiago Prohaska <thiago.prohaska@gmail.com>
2023-12-22 09:42:18 +0100

    Create player component

---
+++ src/components/player/Player.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(name);
+
+    function HandleEditClick() {
+        setIsEditing((editing) => !editing);
+    }
+
+    function HandleOnChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let displayInput = <span className="player-name">{playerName}</span>;
+    if (isEditing)
+        displayInput = <input type="text" id="inputName" required defaultValue={playerName} onChange={HandleOnChange} />;
+
+    return (<li>
+        <span className="palyer">
+            {displayInput}
+            <spam className="player-symbol">{symbol}</spam>
+            <button onClick={HandleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+        </span>
+    </li>);
+}
+
+export default Player;
